d38c8f797394aff351cf5fd114b9ea2c30ed74b5
trevogrif7 <trevogrif7@gmail.com>
2016-10-18 08:51:16 -0500

    Version before branch

--- NerdNumbers/ViewController.swift
+++ NerdNumbers/ViewController.swift
@@ -14,10 +14,33 @@ class ViewController: UIViewController, UIPopoverPresentationControllerDelegate 
     @IBOutlet var myButtons: [UIButton]!
     @IBOutlet weak var difficultyLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var introImageView: UIImageView!
+    
+    // Variable for setting up opening annimation
+    var introMask : CALayer? = CALayer()
         
     override func viewDidLoad() {
         super.viewDidLoad()
+  /*
+        // Set up opening animation
+        //introMask = CALayer()
+        introMask!.contents = UIImage(named: "MiddleOfIcon")?.cgImage
+        introMask!.contentsGravity = kCAGravityResizeAspect
+        introMask!.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
+        introMask!.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        introMask!.position = CGPoint(x: view.frame.size.width/2, y: view.frame.size.height/2)
         
+        // Add intro picture as mask
+        self.introImageView.layer.mask = introMask
+        
+        // Add surrounding color
+        //self.introImageView.backgroundColor = UIColor(red: 0, green: 102/255, blue: 255/255, alpha: 1.0)
+        
+        // Call function to animate app intro
+        animate()
+        
+ 
+ */
         // Format the labels
         difficultyLabel.font = UIFont (name: "ArialRoundedMTBold", size: 28)
         difficultyLabel.alpha = 0
@@ -51,6 +74,31 @@ class ViewController: UIViewController, UIPopoverPresentationControllerDelegate 
 //        for button in myButtons {
 //            button.fadeInView()
 //        }
+    }
+    
+    func animate() {
+        let keyFrameAnimation = CAKeyframeAnimation(keyPath: "bounds")
+        //keyFrameAnimation.delegate = self
+        keyFrameAnimation.duration = 1
+        keyFrameAnimation.beginTime = CACurrentMediaTime() + 1
+        
+        // Begin the animation
+        let firstBounds = NSValue(cgRect:introMask!.bounds)
+        
+        // These parameters create a zoom out then zoom in effect
+        let midBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 90, height: 90))
+        let lastBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 1500, height: 1500))
+        
+        // Add values to key frame animation
+        keyFrameAnimation.values = [firstBounds, midBounds, lastBounds]
+        keyFrameAnimation.keyTimes = [0, 0.4, 1]
+        
+        // Define animations
+        keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)]
+        
+        // Create animation
+        self.introMask?.add(keyFrameAnimation, forKey: "bounds")
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
